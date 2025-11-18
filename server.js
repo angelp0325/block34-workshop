fb010b02ce6f68df5c6873503a8035249837b1fe
Angel Polanco <angelepolanco325@gmail.com>
2025-11-18 17:03:23 -0500

    complete employee directory api with all routes

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 import app from "./app.js";
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
-=======
-import app from "#app";
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
->>>>>>> 2a9c3ac3033b542f20ddb3f8b62de9b9ba6e8cec
 });
